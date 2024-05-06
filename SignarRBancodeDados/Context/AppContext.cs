ab44efdad4b77f1b9d1290c60a6223ffb8fa71b3
Daniel Rosa da Silveira <45170832+danielrosasilveira@users.noreply.github.com>
2024-05-06 15:39:07 -0300

    tempo final do video https://youtu.be/3SJrag0UiTY?t=378

---
+++ SignarRBancodeDados/Context/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignarRBancodeDados.Models;
+
+namespace SignarRBancodeDados.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ SignarRBancodeDados/Repositories/ProdutoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignarRBancodeDados.Models;
+using System.Data;
+
+namespace SignarRBancodeDados.Repositories
+{
+    public class ProdutoRepository
+    {
+        private readonly SignarRBancodeDados.Context.AppContext _context;
+        
+        public ProdutoRepository(Context.AppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Produto>> GetProductDetailsFromDb()
+        {
+            var produtos = await _context.Produtos.ToListAsync();
+            return produtos;
+        }
+    }
+}
